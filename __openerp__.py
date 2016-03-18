e1a81ba2c443f94242a7d1f3c63039b5d740eeb3
David Diz Oya <ddizoya@danielcastelao.org>
2016-03-18 16:12:12 +0100

    Commit inicial del tutorial de Odoo 9

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+  'name':'Tema Tutorial de David Diz',
+  'description': 'Pruebas para Odoo',
+  'version':'1.0',
+  'author':'David Diz',
+
+ 'data': [
+  'views/layout.xml',
+  'views/pages.xml',
+  'views/vacaciones.xml',
+  'views/assets.xml'
+],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
